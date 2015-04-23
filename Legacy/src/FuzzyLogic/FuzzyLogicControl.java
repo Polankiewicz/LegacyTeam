af6d27f61c9290ec2b7da13ae6b050b91b475c40
Polankiewicz <damianpol2@o2.pl>
2015-04-24 00:23:56 +0200

    fuzzyLogic example

---
+++ Legacy/src/FuzzyLogic/FuzzyLogicControl.java
@@ -0,0 +1,31 @@
+package FuzzyLogic;
+
+import net.sourceforge.jFuzzyLogic.FIS;
+
+
+public class FuzzyLogicControl {
+
+	
+	public static void main(String[] args) throws Exception {
+        
+        String fileName = "src/FuzzyLogic/tipper.fcl";
+        FIS fis = FIS.load(fileName,true);
+
+        if( fis == null ) { 
+            System.err.println("Can't load file: '" + fileName + "'");
+            return;
+        }
+
+
+        fis.setVariable("myUnits", 8);
+        fis.setVariable("enemyUnits", 2);
+
+        fis.evaluate();
+
+        
+        System.out.println("Magiczna wartosc: \n" + fis.getVariable("fightResult").getLatestDefuzzifiedValue() );
+        
+        //System.out.println(fis);
+    }
+	
+}
