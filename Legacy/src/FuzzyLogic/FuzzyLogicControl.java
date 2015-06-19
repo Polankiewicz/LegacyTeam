f401619fdff17f4001e1d7314d9de507f6d436e3
Polankiewicz <damianpol2@o2.pl>
2015-06-19 02:37:54 +0200

    FuzzyLogic

--- Legacy/src/FuzzyLogic/FuzzyLogicControl.java
+++ Legacy/src/FuzzyLogic/FuzzyLogicControl.java
@@ -1,36 +1,21 @@
 package FuzzyLogic;
 
 import net.sourceforge.jFuzzyLogic.FIS;
-import net.sourceforge.jFuzzyLogic.FunctionBlock;
-import net.sourceforge.jFuzzyLogic.plot.JFuzzyChart;
-import net.sourceforge.jFuzzyLogic.rule.Variable;
 
 
 public class FuzzyLogicControl {
 
-	private FIS fightChancesFCL;
 	private FIS fieldsControledFCL;
-	private FIS unitsPerFieldFCL;
-	private FIS unitsRatioToBaseFCL;
-	
-	
-	public FuzzyLogicControl(String fightChancesFCL, String fieldsControledFCL,
-			String unitsPerFieldFCL, String unitsRatioToBaseFCL) 
+
+	public FuzzyLogicControl(String fieldsControledFCL) 
 	{
-		loadFclFiles(fightChancesFCL, fieldsControledFCL, unitsPerFieldFCL, unitsRatioToBaseFCL);
+		loadFclFiles(fieldsControledFCL);
 	}
 	
-	private void loadFclFiles(String fightChancesFCL, String fieldsControledFCL,
-			String unitsPerFieldFCL, String unitsRatioToBaseFCL) 
+	private void loadFclFiles(String fieldsControledFCL) 
 	{
 		// values <0,25> - number of fields
 		this.fieldsControledFCL = FIS.load(fieldsControledFCL, true);
-		// values <0,100> - % my units to enemy units
-		this.fightChancesFCL = FIS.load(fightChancesFCL, true);
-		// values <0,100> - % my all units to units on my one field
-		this.unitsPerFieldFCL = FIS.load(unitsPerFieldFCL, true);
-		// values <0,100> - % units in my base to all my units
-		this.unitsRatioToBaseFCL = FIS.load(unitsRatioToBaseFCL, true);
 	}
 	
 	
@@ -41,36 +26,6 @@ public class FuzzyLogicControl {
 		fieldsControledFCL.evaluate();
 		
 		return fieldsControledFCL.getVariable("fieldsRatio").getValue();
-	}
-	
-	public double getFuzzyFightChances(double myUnits, double enemyUnits)
-	{
-        fightChancesFCL.setVariable("myUnits", myUnits);
-        fightChancesFCL.setVariable("enemyUnits", enemyUnits);
-
-        fightChancesFCL.evaluate();
-		
-		return fightChancesFCL.getVariable("fightResult").getValue();
-	}
-	
-	public double getFuzzyUnitsPerField(double allUnits, double unitsOnField)
-	{
-		unitsPerFieldFCL.setVariable("allUnits", allUnits);
-		unitsPerFieldFCL.setVariable("unitsOnField", unitsOnField);
-
-		unitsPerFieldFCL.evaluate();
-		
-		return unitsPerFieldFCL.getVariable("unitsRatio").getValue();
-	}
-	
-	public double getFuzzyUnitsRatioToBase(double unitsInBase, double otherOurUnits)
-	{
-		unitsRatioToBaseFCL.setVariable("unitsInBase", unitsInBase);
-		unitsRatioToBaseFCL.setVariable("otherOurUnits", otherOurUnits);
-
-		unitsRatioToBaseFCL.evaluate();
-		
-		return unitsRatioToBaseFCL.getVariable("result").getValue();
 	}
 	
 }
