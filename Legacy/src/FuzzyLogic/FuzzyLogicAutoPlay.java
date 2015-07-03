cf97e158a40eb65898a5403f68377d6eb93b5454
Dobrotek <dobrotek6@gmail.com>
2015-07-03 14:25:41 +0200

    Mah agent, stronk as mah morale

--- Legacy/src/FuzzyLogic/FuzzyLogicAutoPlay.java
+++ Legacy/src/FuzzyLogic/FuzzyLogicAutoPlay.java
@@ -49,7 +49,7 @@ public class FuzzyLogicAutoPlay {
 		
 		// Auto Players: Damian, Dopek, Polka, Grzesio, Dziedzic, Dobrotek
 		autoBluePlayer = new String("Dobrotek");
-		autoRedPlayer = new String("Dobrotek");
+		autoRedPlayer = new String("Dopek");
 		
 		// Blue Player FCL files
 		this.fuzzyLogicControlForBluePlayer = new FuzzyLogicControl(
